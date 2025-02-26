b4c9c557bc32d1b3fddb85f82c829c25230d548c
Prem Kumar <premkumar@Prems-MacBook-Pro.local>
2025-02-26 10:33:16 -0600

    api setup

---
+++ api/tests/server.test.ts
@@ -0,0 +1,34 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { gql } from 'graphql-tag';
+import typeDefs from '../graphql/typeDefs';
+import resolvers from '../graphql/resolvers';
+
+describe('Apollo Server', () => {
+  let server: ApolloServer;
+
+  beforeAll(async () => {
+    server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+    await startStandaloneServer(server, { listen: { port: 0 } });
+  });
+
+  afterAll(async () => {
+    await server.stop();
+  });
+
+  it('should return hello world', async () => {
+    const result = await server.executeOperation({
+      query: gql`
+        query {
+          hello
+        }
+      `,
+    });
+
+    expect(result.errors).toBeUndefined();
+    expect(result.data?.hello).toBe('Hello world!');
+  });
+});
